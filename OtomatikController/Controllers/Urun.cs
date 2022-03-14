b73438cda6a38b3823c6e49bc87bae6d9efa3700
Meryemnursikak <meryemnursikak@gmail.com>
2022-03-14 14:54:29 +0300

    Add project files.

---
+++ OtomatikController/Controllers/Urun.cs
@@ -0,0 +1,9 @@
+﻿namespace OtomatikController.Controllers
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string UrunAd { get; set; }
+        public decimal Fiyat { get; set; }
+    }
+}
